202b444319f8d4b5ff393f16446b9be0e7e10d6b
sayful <mdsayfulislam1411@gmail.com>
2024-02-17 12:02:46 +0600

    created experience model and migration

---
+++ app/Models/Experience.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Experience extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'company_name',
+        'department',
+        'join',
+        'resign',
+        'is_currently_working',
+        'company_address',
+        'responsibilities'
+    ];
+}
---
+++ database/migrations/2024_02_17_055928_create_experiences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('company_name');
+            $table->string('department');
+            $table->date('join');
+            $table->date('resign')->nullable();
+            $table->boolean('is_currently_working')->default(0);
+            $table->string('company_address');
+            $table->text('responsibilities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
