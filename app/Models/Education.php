15eeedbec9549605c644c65624d3850fe1ab4510
sayful <mdsayfulislam1411@gmail.com>
2024-02-17 11:53:45 +0600

    created education model and migration

---
+++ app/Models/Education.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Education extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'level',
+        'degree_name',
+        'major',
+        'institute_name',
+        'result',
+        'passing_year'
+    ];
+}
---
+++ database/migrations/2024_02_17_054734_create_educations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('educations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('level');
+            $table->string('degree_name');
+            $table->string('major');
+            $table->string('institute_name');
+            $table->string('result');
+            $table->year('passing_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
