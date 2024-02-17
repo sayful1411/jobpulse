85ecb3016d1c9b6fa886e8afc488ffc0ff14daed
sayful <mdsayfulislam1411@gmail.com>
2024-02-17 11:47:11 +0600

    created user profile model and migration

---
+++ app/Models/UserProfile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'father_name',
+        'mother_name',
+        'date_of_birth',
+        'gender',
+        'blood_group',
+        'nid',
+        'phone_2',
+        'address'
+    ];
+}
---
+++ database/migrations/2024_02_17_053910_create_user_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('father_name')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->string('blood_group')->nullable();
+            $table->integer('nid');
+            $table->string('phone_2')->nullable();
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
