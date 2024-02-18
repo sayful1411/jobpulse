125dd4de6d2b384cadfbd928510e78393e4999cd
sayful <mdsayfulislam1411@gmail.com>
2024-02-18 20:34:18 +0600

    some modification in blade template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,64 +2,81 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <title> @yield('title') </title>
+    <meta charset="utf-8">
+    <title> @yield('title') </title>
 
-  <!-- Stylesheets -->
-  <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
+    <!-- Stylesheets -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
 
-  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
-  <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
 
-  <!-- Responsive -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <style>
+        .sidebar-menu-container {
+            display: none;
+        }
+
+        @media only screen and (max-width: 1365px) {
+            .sidebar-menu-container {
+                display: block;
+            }
+        }
+    </style>
 
 </head>
 
 <body data-anm=".anm">
 
-  <div class="page-wrapper">
+    <div class="page-wrapper">
 
-    <!-- Preloader -->
-    <div class="preloader"></div>
+        <!-- Preloader -->
+        <div class="preloader"></div>
 
-    <!-- Main Header-->
-    @guest
-      @include('partials.header')
-    @endguest
+        <!-- Main Header-->
+        @guest
+            @include('partials.header')
+        @endguest
 
-    @auth
-      @include('partials.dashboard-header')
-    @endauth
-    <!--End Main Header -->
+        @auth
+            <div class="sidebar-backdrop"></div>
+            @include('partials.dashboard-header')
+            <div class="sidebar-menu-container">
+                @include('partials.candidate.sidebar-menu')
+            </div>
+        @endauth
+        <!--End Main Header -->
 
-    <!-- Main Section -->
-    @yield('content')
-    <!-- End Main Section -->
+        <!-- Main Section -->
+        @yield('content')
+        <!-- End Main Section -->
 
-    <!-- Main Footer -->
-    @include('partials.footer')
-    <!-- End Main Footer -->
-
-
-  </div><!-- End Page Wrapper -->
+        <!-- Main Footer -->
+        @include('partials.footer')
+        <!-- End Main Footer -->
 
 
-  <script src="{{ asset('js/jquery.js') }}"></script>
-  <script src="{{ asset('js/popper.min.js') }}"></script>
-  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.modal.min.js') }}"></script>
-  <script src="{{ asset('js/mmenu.polyfills.js') }}"></script>
-  <script src="{{ asset('js/mmenu.js') }}"></script>
-  <script src="{{ asset('js/appear.js') }}"></script>
-  <script src="{{ asset('js/ScrollMagic.min.js') }}"></script>
-  <script src="{{ asset('js/rellax.min.js') }}"></script>
-  <script src="{{ asset('js/owl.js') }}"></script>
-  <script src="{{ asset('js/wow.js') }}"></script>
-  <script src="{{ asset('js/script.js') }}"></script>
+    </div><!-- End Page Wrapper -->
+
+
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.modal.min.js') }}"></script>
+    <script src="{{ asset('js/mmenu.polyfills.js') }}"></script>
+    <script src="{{ asset('js/mmenu.js') }}"></script>
+    <script src="{{ asset('js/appear.js') }}"></script>
+    <script src="{{ asset('js/ScrollMagic.min.js') }}"></script>
+    <script src="{{ asset('js/rellax.min.js') }}"></script>
+    <script src="{{ asset('js/owl.js') }}"></script>
+    <script src="{{ asset('js/wow.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 
 </body>
+
 </html>
