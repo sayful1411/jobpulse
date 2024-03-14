439e3ba76c8d99d933b5f7b0963c0d9d36b8f207
sayful <mdsayfulislam1411@gmail.com>
2024-03-14 11:05:32 +0600

    define model migration for manage job application

---
+++ app/Enums/JobApplicationStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+class JobApplicationStatus
+{
+    const PENDING = 'pending';
+    const APPROVED = 'approved';
+    const REJECTED = 'rejected';
+}
---
+++ database/migrations/2024_03_14_050010_create_job_applications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\JobApplicationStatus;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('job_id')->constrained()->cascadeOnDelete();
+            $table->string('status')->default(JobApplicationStatus::PENDING);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_applications');
+    }
+};
